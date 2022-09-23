bc75a33059b4374bf93e404edcf177dd809f0993
Mohammad Mobeen <mobeeen2@outlook.com>
2022-09-23 14:05:56 +0500

    first app push

---
+++ src/Components/MainComponent.js
@@ -0,0 +1,14 @@
+import React from "react"
+export function MainComponent(){
+    return(
+        <div>
+            <h1 className="heading">Reasons for you to follow</h1>
+            <ol>
+                <li> first item </li>
+                <li> second item</li>
+                <li> third item</li>
+                <li> fourth item</li>
+            </ol>
+        </div>
+    )
+}
